5727ddd04591941503a3e59979cfdf0e13fb524d
Maria Garcia <licgarcia_psic@hotmail.com>
2023-07-19 12:48:39 -0600

    reestructuracion Portafolio

--- src/index.js
+++ src/index.js
@@ -1,75 +1,28 @@
-// Obtengo una referencia al botón de descarga
-const downloadButton = document.getElementById("downloadButton");
+let menuVisible = false;
+//Función que oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //oculto el menu una vez que selecciono una opcion
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+
+const downloadButton = document.getElementById("bttnDownload");
 
 // Agrego un evento de clic al botón
 downloadButton.addEventListener("click", () => {
   // URL del archivo PDF
-  const pdfUrl = "cv_MariaGarcia.pdf";
+  const pdfUrl = "img/cv_MariaGarcia.pdf";
 
   // Descargo el archivo
   window.open(pdfUrl, "_blank");
-});
-
-// Obtengo una referencia a los elementos del menú
-const inicioLink = document.querySelector('a[href="#inicio"]');
-const sobreMiLink = document.querySelector('a[href="#sobreMi"]');
-const portafolioLink = document.querySelector('a[href="#portafolio"]');
-const contactoLink = document.querySelector('a[href="#contacto"]');
-
-// Agrego los eventos de clic a los elementos del menú
-inicioLink.addEventListener("click", () => scrollToSection("inicio"));
-sobreMiLink.addEventListener("click", () => scrollToSection("sobreMi"));
-portafolioLink.addEventListener("click", () => scrollToSection("portafolio"));
-contactoLink.addEventListener("click", () => scrollToSection("contacto"));
-
-// Desplacarse de manera armónica
-function scrollToSection(sectionId) {
-  const section = document.getElementById(sectionId);
-  section.scrollIntoView({ behavior: "smooth" });
-}
-
-// Referencia a los elementos de los iconos
-const githubIcons = document.querySelectorAll(
-  '.card__proyect-links a[href^="https://github.com/MariaGarciaB/DEV004-card-validation.github.io"]'
-);
-const websiteIcons = document.querySelectorAll(
-  '.card__proyect-links a[href^="https://mariagarciab.github.io/DEV004-card-validation.github.io/src/"]'
-);
-const codeIcons = document.querySelectorAll(
-  '.card__proyect-links a[href^="https://github.com/MariaGarciaB/DEV004-card-validation.github.io/tree/main/src"]'
-);
-const linkedinIcon = document.getElementById("linkedin-icon");
-// clic a los iconos de GitHub
-githubIcons.forEach((icon) => {
-  icon.addEventListener("click", (event) => {
-    event.preventDefault();
-    const githubLink = event.target.parentNode.href;
-    window.open(githubLink, "_blank");
-  });
-});
-
-// clic a los iconos de sitios web
-websiteIcons.forEach((icon) => {
-  icon.addEventListener("click", (event) => {
-    event.preventDefault();
-    const websiteLink = event.target.parentNode.href;
-    window.open(websiteLink, "_blank");
-  });
-});
-
-// clic a los iconos de código
-codeIcons.forEach((icon) => {
-  icon.addEventListener("click", (event) => {
-    event.preventDefault();
-    const codeLink = event.target.parentNode.href;
-    window.open(codeLink, "_blank");
-  });
-});
-
-// Agrego el evento de clic a cada ícono
-// githubIcon.addEventListener('click', () => {
-//   window.open('https://github.com/MariaGarciaB', '_blank');
-// });
-linkedinIcon.addEventListener("click", () => {
-  window.open("https://www.linkedin.com/in/mariagarbar/", "_blank");
 });
